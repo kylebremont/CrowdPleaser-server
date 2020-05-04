2f3aa4a89c09b5a0d297d3a9b16c3b63b808ab1d
= <matthewcb11@gmail.com>
2020-05-03 23:32:16 -0600

    Added endpoint to retrieve device id

--- app.js
+++ app.js
@@ -8,6 +8,15 @@ const MongoClient = require('mongodb').MongoClient;
 
 app.use(bodyParser.json());
 
+// allow cross-origin requests
+let allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Headers', "*");
+    res.header('Access-Control-Allow-Methods', "*");
+    next();
+  }
+app.use(allowCrossDomain);
+
 
 const uri = "mongodb+srv://Server:Crowdpleaser!@crowd-cluster-mvc86.gcp.mongodb.net/test?retryWrites=true&w=majority"
 mongo.connect(uri,  { useNewUrlParser: true, useUnifiedTopology: true }, function(err, db) {
@@ -28,9 +37,10 @@ mongo.connect(uri,  { useNewUrlParser: true, useUnifiedTopology: true }, functio
     app.put('/devices', (req, res) => {
         // add to db
         for (key in req.body) {
-            var device = JSON.parse(JSON.stringify({"_id": key, "access_token": req.body.device_id}))
+            console.log(req.body[key])
+            // console.log(req.body.device_id[key])
+            var device = JSON.parse(JSON.stringify({"_id": key, "access_token": req.body[key]}))
         }
-
         dbo.collection("devices").find(device).toArray()
         .then(results => {
             if (results.length !== 0) {
@@ -43,6 +53,15 @@ mongo.connect(uri,  { useNewUrlParser: true, useUnifiedTopology: true }, functio
                 res.status(200).send("Device added to DB");
             }
             
+        });
+    })
+
+
+    app.get('/access_token', (req, res) => {
+        // search db for device_id
+        dbo.collection("devices").find(req.query.device_id).toArray()
+        .then(results => {
+            res.status(200).send(results[0].access_token);
         });
     })
 });
