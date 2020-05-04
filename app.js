0f3284bc400b9710d19f004960ad400fdedb6e92
= <matthewcb11@gmail.com>
2020-05-03 18:47:07 -0600

    Added nodemon and initalized mongo connection

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require('express')
 const app = express()
-const port = 3500
+var mongo = require('mongodb');
+const port = 3400
+// import pw from './config'
+
+
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://Server:Crowdpleaser!@crowd-cluster-mvc86.gcp.mongodb.net/test?retryWrites=true&w=majority"
+mongo.connect(uri,  { useNewUrlParser: true, useUnifiedTopology: true }, function(err, db) {
+    if(err) {
+        console.log('Sorry unable to connect to MongoDB Error:', err);
+    }
+    console.log("Connected to mongodb");
+});
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
